f7c94661f02e05c5c300d0817879896ac0884175
Saúl Romero Cruz <saulromero200323@gmail.com>
2026-09-07 13:40:49 -0600

    refactor(vue): use slot in title component

--- vue-nuxt/app/components/TheTitle.vue
+++ vue-nuxt/app/components/TheTitle.vue
@@ -1,11 +1,5 @@
-<script setup lang="ts">
-defineProps<{
-  propTitle: string
-}>()
-</script>
-
 <template>
   <h1 class="text-2xl font-bold text-primary">
-    {{ propTitle }}
+    <slot />
   </h1>
 </template>
