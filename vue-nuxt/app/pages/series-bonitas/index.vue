2130fde704363321624dbb6137a57f8b26db5ef3
Saúl Romero Cruz <saulromero200323@gmail.com>
2026-09-08 10:44:30 -0600

    feat(nuxt): add dynamic series routes

---
+++ vue-nuxt/app/pages/series-bonitas/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <main class="p-6">
+    <TheTitle>Series bonitas</TheTitle>
+
+    <p class="mt-4">
+      Estás en la página: Series bonitas
+    </p>
+
+    <NuxtLink
+      class="btn mt-4 inline-block"
+      to="/series-bonitas/aventuras"
+    >
+      Aventuras
+    </NuxtLink>
+  </main>
+</template>
