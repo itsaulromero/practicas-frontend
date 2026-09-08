5b36131d263becbc40fb196db66d879fc75fa612
Saúl Romero Cruz <saulromero200323@gmail.com>
2026-09-08 09:16:06 -0600

    feat(vue): add computed doubled counter

--- vue-nuxt/app/components/TheCounter.vue
+++ vue-nuxt/app/components/TheCounter.vue
@@ -1,19 +1,28 @@
 <script setup lang="ts">
+import { computed } from 'vue'
+
 const {
   count,
   increment,
   decrement,
   reset,
 } = useCounter()
+
+const doubledCount = computed(() => count.value * 2)
 </script>
 
 <template>
   <section>
     <TheTitle>Counter</TheTitle>
 
+    <TheTitle>
+      Doubled Counter: {{ doubledCount }}
+    </TheTitle>
+
     <p :class="{ 'text-green-500': count === 10 }">
       {{ count }}
     </p>
+
     <BaseButton
       v-if="count < 10"
       @click="increment"
