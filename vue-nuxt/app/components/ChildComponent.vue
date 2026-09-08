38d92ee869b61e3c0e90bffd45277bc27d3bc87e
Saúl Romero Cruz <saulromero200323@gmail.com>
2026-09-08 08:49:46 -0600

    feat(vue): add child to parent event communication

---
+++ vue-nuxt/app/components/ChildComponent.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+const emit = defineEmits<{
+  greet: []
+}>()
+</script>
+
+<template>
+  <BaseButton @click="emit('greet')">
+    Greet parent
+  </BaseButton>
+</template>
---
+++ vue-nuxt/app/components/ParentComponent.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const greeting = ref('')
+
+const handleGreeting = () => {
+  greeting.value = 'Hello from the child component!'
+}
+</script>
+
+<template>
+  <section>
+    <TheTitle>Emit Example</TheTitle>
+
+    <ChildComponent @greet="handleGreeting" />
+
+    <p v-if="greeting">
+      {{ greeting }}
+    </p>
+  </section>
+</template>
